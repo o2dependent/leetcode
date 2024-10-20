804e8444c9876b8378a6abec2b76d64a39f57752
Ethan Olsen <131eolsen@gmail.com>
2024-10-20 08:08:53 -0700

    feat: valid parentheses

---
+++ problems/validParentheses.ts
@@ -0,0 +1,33 @@
+/**
+ * Push and pop stack DS to make sure everything lines up properly.
+ * I know there is a "longest valid parentheses" problem that this would be the base for
+ * and I think for that you'd just wait for the stack to empty then update a "max" var
+ */
+
+const OPENING = new Map([
+	["[", 1],
+	["(", 2],
+	["{", 3],
+]);
+const CLOSING = new Map([
+	["]", 1],
+	[")", 2],
+	["}", 3],
+]);
+
+function isValid(s: string): boolean {
+	const stack: string[] = [];
+
+	for (let i = 0; i < s.length; i++) {
+		const val = s[i];
+		if (OPENING.get(val)) stack.push(val);
+		else if (CLOSING.get(val)) {
+			if (OPENING.get(stack?.pop() ?? "") !== CLOSING.get(val)) return false;
+		}
+	}
+	return stack.length === 0;
+}
+
+console.log(isValid("()"));
+console.log(isValid("(]"));
+console.log(isValid("([])"));
