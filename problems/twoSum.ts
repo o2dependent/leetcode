5e18bf011fc544ec42a4ab6cfa57d6976c4bf825
Ethan Olsen <131eolsen@gmail.com>
2024-10-20 08:17:49 -0700

    chore: update two sum to better solution with comments

--- problems/twoSum.ts
+++ problems/twoSum.ts
@@ -1,15 +1,18 @@
 import { analyzePerformance } from "../utils/analyzePerformance";
 
-const twoSumInteresting = (nums: number[], target: number): number[] => {
+function twoSum(nums: number[], target: number): number[] {
 	const map = new Map();
 	for (let i = 0; i < nums.length; i++) {
-		if (map.has(target - nums[i])) return [map.get(target - nums[i]), i];
-		else map.set(nums[i], i);
+		const val1 = nums[i];
+		// check if the number we need is here and return indices if true
+		if (map.has(target - val1)) return [map.get(target - val1), i];
+		// otherwise lets set that value to the current index
+		else map.set(val1, i);
 	}
 	return [];
-};
+}
 
-const twoSum = (nums: number[], target: number): number[] => {
+const BRUTEtwoSum = (nums: number[], target: number): number[] => {
 	for (let i = 0; i < nums.length; i++) {
 		const val1 = nums[i];
 		for (let j = 0; j < nums.length - i - 1; j++) {
@@ -21,4 +24,4 @@ const twoSum = (nums: number[], target: number): number[] => {
 };
 
 analyzePerformance(() => twoSum([2, 7, 11, 15], 9));
-analyzePerformance(() => twoSumInteresting([2, 7, 11, 15], 9));
+analyzePerformance(() => BRUTEtwoSum([2, 7, 11, 15], 9));
