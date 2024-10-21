bad77f17389da621fbb3b41d66cec1298b4ab02a
Ethan Olsen <131eolsen@gmail.com>
2024-10-21 01:42:21 -0700

    feat: trapping rain water

--- problems/trappingRainWater.ts
+++ problems/trappingRainWater.ts
@@ -42,5 +42,24 @@ function trap(height: number[]): number {
 	return ans;
 }
 
+function trapSTACK_ANSWER(height: number[]): number {
+	let ans = 0;
+	let current = 0;
+	let st = [];
+	while (current < height.length) {
+		while (st.length !== 0 && height[current] > height[st[st.length - 1]]) {
+			let top = st.pop()!;
+			if (st.length === 0) break;
+			let distance = current - st[st.length - 1] - 1;
+			let bounded_height =
+				Math.min(height[current], height[st[st.length - 1]]) - height[top];
+			ans += distance * bounded_height;
+		}
+		st.push(current++);
+	}
+	return ans;
+}
+
 console.log(trap([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]));
+console.log(trapSTACK_ANSWER([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]));
 // console.log(trap([4, 2, 0, 3, 2, 5]));
