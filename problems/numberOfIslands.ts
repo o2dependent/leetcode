333c8929becea28fc7d52a587576b4105cfaab8d
Ethan Olsen <131eolsen@gmail.com>
2024-10-23 15:18:41 -0700

    feat: more efficient number of island solution

--- problems/numberOfIslands.ts
+++ problems/numberOfIslands.ts
@@ -1,13 +1,44 @@
+// function numIslands(grid: string[][]): number {
+// 	const map = new Map();
+// 	const m = grid?.length ?? 0;
+// 	const n = grid?.[0]?.length ?? 0;
+// 	const islandChar = "1";
+// 	const waterChar = "0";
+
+// 	const findIsland = (i: number, j: number) => {
+// 		if (map.get(`${i}-${j}`) || grid?.[i]?.[j] !== islandChar) return 0;
+// 		map.set(`${i}-${j}`, true);
+// 		// check directions
+// 		findIsland(i + 1, j);
+// 		findIsland(i - 1, j);
+// 		findIsland(i, j + 1);
+// 		findIsland(i, j - 1);
+// 		return 1;
+// 	};
+
+// 	let numIslands = 0;
+
+// 	for (let i = 0; i < m; i++) {
+// 		for (let j = 0; j < n; j++) {
+// 			numIslands += findIsland(i, j);
+// 		}
+// 	}
+// 	return numIslands;
+// }
+
+/**
+ * Removed visited map for mutating original array
+ */
 function numIslands(grid: string[][]): number {
-	const map = new Map();
 	const m = grid?.length ?? 0;
 	const n = grid?.[0]?.length ?? 0;
 	const islandChar = "1";
 	const waterChar = "0";
+	const visitedChar = "V";
 
 	const findIsland = (i: number, j: number) => {
-		if (map.get(`${i}-${j}`) || grid?.[i]?.[j] !== islandChar) return 0;
-		map.set(`${i}-${j}`, true);
+		if (grid?.[i]?.[j] !== islandChar) return 0;
+		grid[i][j] = visitedChar;
 		// check directions
 		findIsland(i + 1, j);
 		findIsland(i - 1, j);
